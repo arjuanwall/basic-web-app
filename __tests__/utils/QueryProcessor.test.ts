d53ecc12f9ca28bcf31216af2f1259030f7b676a
arjunw <walia.arjun.2004@gmail.com>
2025-02-24 11:36:46 -0500

    added andrewid tests

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -15,7 +15,15 @@ describe("QueryProcessor", () => {
             "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
             "English poet, playwright, and actor, widely regarded as the greatest " +
             "writer in the English language and the world's pre-eminent dramatist."
-          ));
+        ));
+    });
+
+    test('should return Andrew ID', () => {
+        const query = "What is your andrew id?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "arjunw"
+        ));
     });
 
     test('should return name', () => {
@@ -23,6 +31,6 @@ describe("QueryProcessor", () => {
         const response: string = QueryProcessor(query);
         expect(response).toBe((
             "Rohan"
-          ));
+        ));
     })
 });
